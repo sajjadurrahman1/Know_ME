8a30c91cfbdd8cf66796e5981365660fe0075bcd
sajjad <sajjadurrahman130784@gmail.com>
2025-10-05 15:45:19 +0200

    Hero section

---
+++ portfolio.js
@@ -0,0 +1,9 @@
+// 1. Typing Animation for Hero Section
+const roles = ["Emerging Tech Enthusiast", "B.Tech Student", "Frontend Developer", "Backend Developer"];
+let roleIndex = 0;
+let charIndex = 0;
+const typingSpeed = 100;
+const erasingSpeed = 50;
+const pauseTime = 2000;
+
+const typedText = document.querySelector(".intro h3");
